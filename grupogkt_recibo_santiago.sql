0b4bebcc2059aa7e9eebbd181219f3f0d972441a
RafiquiGeek <56735459+RafiquiGeek@users.noreply.github.com>
2024-10-10 20:34:08 -0500

    Add files via upload

---
+++ grupogkt_recibo_santiago.sql
@@ -0,0 +1,116 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost:3306
+-- Tiempo de generación: 11-10-2024 a las 07:03:51
+-- Versión del servidor: 5.7.23-23
+-- Versión de PHP: 8.1.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `grupogkt_recibo_santiago`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `constancia_pago`
+--
+
+CREATE TABLE `constancia_pago` (
+  `ID_RECIBO` int(11) NOT NULL,
+  `cliente_recibo` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `dni_recibo` int(10) NOT NULL,
+  `cuota_recibo` double NOT NULL,
+  `penalidad_recibo` double NOT NULL,
+  `ga_recibo` double NOT NULL,
+  `fecha_recibo` varchar(52) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `personas`
+--
+
+CREATE TABLE `personas` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `pais` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `edad` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `personas`
+--
+
+INSERT INTO `personas` (`id`, `nombre`, `pais`, `edad`) VALUES
+(1, 'ricardo', 'peru', 34);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `usuario` varchar(25) COLLATE utf8_spanish_ci NOT NULL,
+  `password` varchar(50) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `usuario`, `password`) VALUES
+(1, 'admin', '827ccb0eea8a706c4c34a16891f84e7b'),
+(2, 'demo', 'fe01ce2a7fbac8fafaed7c982a04e229');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `constancia_pago`
+--
+ALTER TABLE `constancia_pago`
+  ADD PRIMARY KEY (`ID_RECIBO`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `usuario` (`usuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `constancia_pago`
+--
+ALTER TABLE `constancia_pago`
+  MODIFY `ID_RECIBO` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
